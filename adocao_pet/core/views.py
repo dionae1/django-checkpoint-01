70f2aaf0b890090d7ff9288c3d39d3b8aa56b618
dionae1 <zguixmz@gmail.com>
2026-06-03 20:01:06 -0300

    fix: corrigir rota de cadastro protegida + corrigir login apontando url errada

--- adocao_pet/core/views.py
+++ adocao_pet/core/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 
 from .forms import FormularioCadastro, FormularioAtualizacao
 
-@login_required
+
 def cadastrar_usuario(request):
 
     if request.method == "POST":
